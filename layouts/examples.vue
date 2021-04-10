f955e4e569649ebb0138cf2a533d7bd6382d1f35
rodrigodjodi <rodrigo.djodi@gmail.com>
2021-04-10 00:27:10 -0300

    checking codesandbox capacity

---
+++ layouts/examples.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <Nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+        height: 100%;
+      }
+body {
+height: 100%;
+overflow: hidden;
+margin: 0;
+padding: 0;
+font-family: Arial, Helvetica, sans-serif;
+font-size: 16px;
+color: #ffffff;
+background-color: #000000;
+}
+
+
+.container {
+    height: 100%;
+}
+
+</style>
---
+++ pages/_example/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="pano" style="width: 100%; height: 600px;"></div>
+</template>
+
+<script>
+
+
+export default {
+  layout: 'examples',
+  head: {
+      script: [
+      { src: 'krpano.js'  }
+    ],
+  },
+  mounted() {
+      embedpano({
+          xml: "krpano.xml",
+        target: "pano",
+        html5: "only",
+        initvars: {
+          editmode: "true"
+        },
+        // onready: krpanoReady,
+        mobilescale: 1.0,
+        passQueryParameters: true
+      });
+  }
+}
+</script>
+
+<style>
+
+</style>
